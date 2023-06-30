fea682acb71ff3e3ccb63715155da6b0c2202673
chojeongeun <juy0816@naver.com>
2023-06-30 12:46:10 +0900

    각 페이지 컴포넌트 셋팅

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import Footer from './components/common/Footer';
+import Header from './components/common/Header';
+
+import Visual from './components/main/Visual';
+
+import Community from './components/sub/Community';
+import Contact from './components/sub/Contact';
+import Department from './components/sub/Department';
+import Gallery from './components/sub/Gallery';
+import Member from './components/sub/Member';
+import Youtube from './components/sub/Youtube';
+
+function App() {
+	return (
+		<>
+			<Header />
+
+			<Visual />
+			<Department />
+			<Community />
+			<Gallery />
+			<Youtube />
+			<Contact />
+			<Member />
+
+			<Footer />
+		</>
+	);
+}
+
+export default App;
