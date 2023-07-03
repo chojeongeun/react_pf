c9a9d9a310d805d0c0bd5b06be73e3bc24b31135
chojeongeun <juy0816@naver.com>
2023-07-03 13:20:17 +0900

    폼양식 비밀번호 재입력까지 틀 완성

--- src/components/sub/Member.js
+++ src/components/sub/Member.js
@@ -1,7 +1,46 @@
 import Layout from '../common/Layout';
 
 function Member() {
-	return <Layout name={'MEMBER'}>Member</Layout>;
+	return (
+		<Layout name={'MEMBER'}>
+			<form>
+				<fieldset>
+					<legend>회원가입 폼 양식</legend>
+					<table border='1'>
+						{/* user id */}
+						<tr>
+							<th scope='row'>
+								<label htmlFor='userid'>USER ID</label>
+							</th>
+							<td>
+								<input type='text' name='userid' id='userid' placeholder='아이디를 입력하세요' />
+							</td>
+						</tr>
+
+						{/* password */}
+						<tr>
+							<th>
+								<label htmlFor='pwd1'>PASSWORD</label>
+							</th>
+							<td>
+								<input type='password' name='pwd1' id='pwd1' placeholder='비밀번호를 입력하세요' />
+							</td>
+						</tr>
+
+						{/* re-password */}
+						<tr>
+							<th>
+								<label htmlFor='pwd2'>RE-PASSWORD</label>
+							</th>
+							<td>
+								<input type='re-password' name='pwd2' id='pwd2' placeholder='비밀번호를 재입력하세요' />
+							</td>
+						</tr>
+					</table>
+				</fieldset>
+			</form>
+		</Layout>
+	);
 }
 
 export default Member;
