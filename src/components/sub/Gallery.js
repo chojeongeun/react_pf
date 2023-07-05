21ef96470cf9d146e3b0a0b119b1b2c52f66a1a6
chojeongeun <juy0816@naver.com>
2023-07-05 15:32:12 +0900

    gallery 작업

--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -1,7 +1,62 @@
 import Layout from '../common/Layout';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import Masonry from 'react-masonry-component';
 
 function Gallery() {
-	return <Layout name={'GALLERY'}>Gallery</Layout>;
+	const [Items, setItems] = useState([]);
+
+	const getFlickr = async (opt) => {
+		const baseURL = 'https://www.flickr.com/services/rest/?format=json&nojsoncallback=1';
+		const key = 'aa7fe17db9f4fb9505a841ac24c4f798';
+		const method_interest = 'flickr.interestingness.getList';
+		const method_user = 'flickr.people.getPhotos';
+		const method_search = 'flick.photos.search';
+		const num = 500;
+		let url = '';
+		if (opt.type === 'interest') url = `${baseURL}&api_key=${key}&method=${method_interest}&per_page=${num}`;
+		if (opt.type === 'search')
+			url = `${baseURL}&api_key=${key}&method=${method_search}&per_page=${num}&tags=${opt.tags}`;
+		if (opt.type === 'user')
+			url = `${baseURL}&api_key=${key}&method=${method_user}&per_page=${num}&user_id=${opt.user}`;
+
+		const result = await axios.get(url);
+		setItems(result.data.photos.photo);
+	};
+
+	useEffect(() => getFlickr({ type: 'interest' }), []);
+
+	return (
+		<Layout name={'GALLERY'}>
+			<div className='frame'>
+				<Masonry elementType={'div'} options={{ transitionDuration: '0.5s' }}>
+					{Items.map((item, idx) => {
+						return (
+							<article key={idx}>
+								<div className='inner'>
+									<div className='pic'>
+										<img
+											src={`https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_m.jpg`}
+											alt={item.title}
+										/>
+									</div>
+									<h2>{item.title}</h2>
+									<div className='profile'>
+										<img
+											src={`http://farm${item.farm}.staticflickr.com/${item.server}/buddyicons/${item.owner}.jpg`}
+											alt={item.owner}
+											onError={(e) => e.target.setAttribute('src', 'https://www.flickr.com/images/buddyicon.gif')}
+										/>
+										<span>{item.owner}</span>
+									</div>
+								</div>
+							</article>
+						);
+					})}
+				</Masonry>
+			</div>
+		</Layout>
+	);
 }
 
 export default Gallery;
