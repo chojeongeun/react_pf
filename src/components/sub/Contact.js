93147dc42b85f1c5ba3586e6324bae46cd9add31
chojeongeun <juy0816@naver.com>
2023-07-15 01:00:09 +0900

    Traffic코드 정리 및 개선 & 지도에 타입, 줌 컨트롤 추가

--- src/components/sub/Contact.js
+++ src/components/sub/Contact.js
@@ -5,16 +5,18 @@ import emailjs from '@emailjs/browser';
 function Contact() {
 	const container = useRef(null);
 	const [Traffic, setTraffic] = useState(false);
-	const form = useRef(null);
-	const inputName = useRef(null);
-	const inputEmail = useRef(null);
-	const inputMsg = useRef(null);
-	const [Success, setSuccess] = useState(false);
+	const [Location, setLocation] = useState(null);
 	const { kakao } = window;
 	const option = {
 		center: new kakao.maps.LatLng(37.56697058676817, 126.97849940948345),
 		level: 3,
 	};
+
+	const form = useRef(null);
+	const inputName = useRef(null);
+	const inputEmail = useRef(null);
+	const inputMsg = useRef(null);
+	const [Success, setSuccess] = useState(false);
 
 	//폼메일 함수
 	const sendEmail = (e) => {
@@ -35,19 +37,23 @@ function Contact() {
 		);
 	};
 
+	const imgSrc = `${process.env.PUBLIC_URL}/img/marker1.png`; // 마커이미지의 주소입니다
+	const imgSize = new kakao.maps.Size(232, 99); // 마커이미지의 크기입니다
+	const imgPos = { offset: new kakao.maps.Point(116, 99) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+	const markerImage = new kakao.maps.MarkerImage(imgSrc, imgSize, imgPos);
+	const marker = new kakao.maps.Marker({ position: option.center, image: markerImage });
+
 	useEffect(() => {
 		const mapInstance = new kakao.maps.Map(container.current, option);
-		const imgSrc = `${process.env.PUBLIC_URL}/img/marker1.png`; // 마커이미지의 주소입니다
-		const imgSize = new kakao.maps.Size(232, 99); // 마커이미지의 크기입니다
-		const imgPos = { offset: new kakao.maps.Point(116, 99) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
-		const markerImage = new kakao.maps.MarkerImage(imgSrc, imgSize, imgPos);
-
-		const marker = new kakao.maps.Marker({ position: option.center, image: markerImage });
-		mapInstance.setZoomable(false);
-
+		mapInstance.setZoomable(false); //찾아보기
 		marker.setMap(mapInstance);
+		mapInstance.addControl(new kakao.maps.MapTypeControl(), kakao.maps.ControlPosition.TOPRIGHT);
+		mapInstance.addControl(new kakao.maps.ZoomControl(), kakao.maps.ControlPosition.RIGHT);
+		setLocation(mapInstance);
+	}, []);
 
-		Traffic ? mapInstance.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC) : mapInstance.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
+	useEffect(() => {
+		Traffic ? Location?.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC) : Location?.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
 	}, [Traffic]);
 
 	return (
