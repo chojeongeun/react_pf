bd9ec4ec6157ab473dea75c1215c45c68e777146
chojeongeun <juy0816@naver.com>
2023-07-14 19:19:29 +0900

    첫번째 버튼 일단 활성화 처리

--- src/components/main/Btns.js
+++ src/components/main/Btns.js
@@ -1,13 +1,17 @@
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useState } from 'react';
+import Anime from '../../asset/anime';
 
 function Btns() {
+	console.log('btn Called');
 	const btnRef = useRef(null);
-	let pos = useRef([]);
+	const pos = useRef([]);
+	const [Num, setNum] = useState(0);
 
 	const getPos = () => {
 		pos.current = [];
 		const secs = btnRef.current.parentElement.querySelectorAll('.myScroll');
 		for (const sec of secs) pos.current.push(sec.offsetTop);
+		setNum(pos.current.length);
 	};
 
 	useEffect(() => {
@@ -21,11 +25,25 @@ function Btns() {
 
 	return (
 		<ul className='btnNavi' ref={btnRef}>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
-			<li></li>
+			{Array(Num)
+				.fill()
+				.map((_, idx) => {
+					let defaultClass = '';
+					if (idx === 0) defaultClass = 'on';
+					return (
+						<li
+							key={idx}
+							className={defaultClass}
+							onClick={() => {
+								new Anime(window, {
+									prop: 'scroll',
+									value: pos.current[idx],
+									duration: 500,
+								});
+							}}
+						></li>
+					);
+				})}
 		</ul>
 	);
 }
