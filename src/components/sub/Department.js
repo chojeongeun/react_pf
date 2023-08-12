cbc716785fd7fabcb84682245023f27d2351f63f
chojeongeun <133828162+chojeongeun@users.noreply.github.com>
2023-08-12 21:52:50 +0900

    department page 컨텐츠 추가

--- src/components/sub/Department.js
+++ src/components/sub/Department.js
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faCloudArrowDown, faHeart, faLaptopCode, faPhone } from '@fortawesome/free-solid-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
 import Layout from '../common/Layout';
@@ -17,24 +17,64 @@ function Department() {
 	}, []);
 	return (
 		<Layout name={'DEPARTMENT'}>
-			{Members.map((member, idx) => {
-				return (
-					<>
-						<article key={idx}>
-							<div className='pic'>
-								<img src={`${process.env.PUBLIC_URL}/img/${member.pic}`} alt={member.name} />
-							</div>
-							<h2>{member.name}</h2>
-							<p>{member.position}</p>
-							<div className='icon'>
-								<FontAwesomeIcon icon={faPhone} />
-								<FontAwesomeIcon icon={faEnvelope} />
-								<FontAwesomeIcon icon={faUser} />
-							</div>
-						</article>
-					</>
-				);
-			})}
+			<section id='upper'>
+				<div class='aboutTxt'>
+					<h2>OUR STORY</h2>
+					<p>
+						There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, ran- domised words which don't look even
+						slightly believable. If you are going to use passage of Lorem, you need to be sure there isn't anything. There are many variations of passages of Lorem Ipsum available, but the majority
+						have suffered alteration in some form, by injected humour, ran- domised words which don't look even slightly believable. If you are going to use passage of Lorem, you need to be sure there
+						isn't anything.
+					</p>
+				</div>
+
+				<div class='pic'>
+					<img src={process.env.PUBLIC_URL + '/img/pic44.jpg'} alt='pic44' />
+				</div>
+			</section>
+
+			<section id='middle'>
+				<article>
+					<FontAwesomeIcon icon={faLaptopCode} />
+					<h3>Web Projects</h3>
+					<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid.</p>
+				</article>
+				<article>
+					<FontAwesomeIcon icon={faHeart} />
+					<h3>Our history</h3>
+					<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid.</p>
+				</article>
+				<article>
+					<FontAwesomeIcon icon={faCloudArrowDown} />
+					<h3>Development</h3>
+					<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid.</p>
+				</article>
+			</section>
+
+			<section id='lower'>
+				<h2>Meet Our Tim</h2>
+				<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil vel repellat architecto. Provident aut vel nesciunt veritatis in, quis ipsum?</p>
+				<div className='wrap'>
+					{Members.map((member, idx) => {
+						return (
+							<>
+								<article key={idx}>
+									<div className='pic'>
+										<img src={`${process.env.PUBLIC_URL}/img/${member.pic}`} alt={member.name} />
+									</div>
+									<h2>{member.name}</h2>
+									<p>{member.position}</p>
+									<div className='icon'>
+										<FontAwesomeIcon icon={faPhone} />
+										<FontAwesomeIcon icon={faEnvelope} />
+										<FontAwesomeIcon icon={faUser} />
+									</div>
+								</article>
+							</>
+						);
+					})}
+				</div>
+			</section>
 		</Layout>
 	);
 }
