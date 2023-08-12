19a42de47ce8a718ab8e83c94d07dc6e33714332
chojeongeun <133828162+chojeongeun@users.noreply.github.com>
2023-08-12 17:10:58 +0900

    header inner추가 및 반응형 수정

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -8,66 +8,70 @@ function Header({ type, menu }) {
 	return (
 		<>
 			<header className={type}>
-				<h1>
-					<Link to='/'>
-						POPULAR
-						<br />
-						SCIENCE
-					</Link>
-				</h1>
+				<div className='inner'>
+					<h1>
+						<Link to='/'>
+							POPULAR
+							<br />
+							SCIENCE
+						</Link>
+					</h1>
 
-				<ul id='gnb1'>
-					<li>
-						<NavLink to='/department' activeClassName={active}>
-							DEPARTMENT
-						</NavLink>
-					</li>
-					<li>
-						<NavLink to='/community' activeClassName={active}>
-							COMMUNITY
-						</NavLink>
-					</li>
-					<li>
-						<NavLink to='/gallery' activeClassName={active}>
-							GALLERY
-						</NavLink>
-					</li>
-				</ul>
+					<ul id='gnb1'>
+						<li>
+							<NavLink to='/department' activeClassName={active}>
+								DEPARTMENT
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to='/community' activeClassName={active}>
+								COMMUNITY
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to='/gallery' activeClassName={active}>
+								GALLERY
+							</NavLink>
+						</li>
+					</ul>
 
-				<ul id='gnb2'>
-					<li>
-						<NavLink to='/youtube' activeClassName={active}>
-							YOUTUBE
-						</NavLink>
-					</li>
-					<li>
-						<NavLink to='/contact' activeClassName={active}>
-							CONTACT
-						</NavLink>
-					</li>
-					<li>
-						<NavLink to='/member' activeClassName={active}>
-							MEMBER
-						</NavLink>
-					</li>
-				</ul>
+					<ul id='gnb2'>
+						<li>
+							<NavLink to='/youtube' activeClassName={active}>
+								YOUTUBE
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to='/contact' activeClassName={active}>
+								CONTACT
+							</NavLink>
+						</li>
 
-				<ul id='util'>
-					<li>
-						<NavLink to='/'>LOG IN</NavLink>
-					</li>
-					<li>
-						<NavLink to='/'>
-							<FontAwesomeIcon icon={faMagnifyingGlass} />
-						</NavLink>
-					</li>
-				</ul>
-				<FontAwesomeIcon
-					icon={faBars}
-					onClick={() => {
-						menu.current.toggle();
-					}}
-				/>
+						<li>
+							<NavLink to='/member' activeClassName={active}>
+								MEMBER
+							</NavLink>
+						</li>
+					</ul>
+
+					<ul id='util'>
+						<li>
+							<NavLink to='/'>LOG IN</NavLink>
+						</li>
+						<li>
+							<NavLink to='/'>
+								<FontAwesomeIcon icon={faMagnifyingGlass} />
+							</NavLink>
+						</li>
+					</ul>
+					<FontAwesomeIcon
+						icon={faBars}
+						className='btnCall'
+						onClick={() => {
+							menu.current.toggle();
+						}}
+					/>
+				</div>
 			</header>
 		</>
 	);
