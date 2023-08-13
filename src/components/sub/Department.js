495aef85f0ddf3b746e7ec11700bda62540e8365
chojeongeun <133828162+chojeongeun@users.noreply.github.com>
2023-08-13 15:01:25 +0900

    member페이지 반응형 수정

--- src/components/sub/Department.js
+++ src/components/sub/Department.js
@@ -17,64 +17,66 @@ function Department() {
 	}, []);
 	return (
 		<Layout name={'DEPARTMENT'}>
-			<section id='upper'>
-				<div class='aboutTxt'>
-					<h2>OUR STORY</h2>
-					<p>
-						There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, ran- domised words which don't look even
-						slightly believable. If you are going to use passage of Lorem, you need to be sure there isn't anything. There are many variations of passages of Lorem Ipsum available, but the majority
-						have suffered alteration in some form, by injected humour, ran- domised words which don't look even slightly believable. If you are going to use passage of Lorem, you need to be sure there
-						isn't anything.
-					</p>
-				</div>
+			<div className='inner'>
+				<section id='upper'>
+					<div class='aboutTxt'>
+						<h2>OUR STORY</h2>
+						<p>
+							There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, ran- domised words which don't look even
+							slightly believable. If you are going to use passage of Lorem, you need to be sure there isn't anything. There are many variations of passages of Lorem Ipsum available, but the majority
+							have suffered alteration in some form, by injected humour, ran- domised words which don't look even slightly believable. If you are going to use passage of Lorem, you need to be sure
+							there isn't anything.
+						</p>
+					</div>
 
-				<div class='pic'>
-					<img src={process.env.PUBLIC_URL + '/img/pic44.jpg'} alt='pic44' />
-				</div>
-			</section>
+					<div class='pic'>
+						<img src={process.env.PUBLIC_URL + '/img/pic44.jpg'} alt='pic44' />
+					</div>
+				</section>
 
-			<section id='middle'>
-				<article>
-					<FontAwesomeIcon icon={faLaptopCode} />
-					<h3>Web Projects</h3>
-					<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid.</p>
-				</article>
-				<article>
-					<FontAwesomeIcon icon={faHeart} />
-					<h3>Our history</h3>
-					<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid.</p>
-				</article>
-				<article>
-					<FontAwesomeIcon icon={faCloudArrowDown} />
-					<h3>Development</h3>
-					<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid.</p>
-				</article>
-			</section>
+				<section id='middle'>
+					<article>
+						<FontAwesomeIcon icon={faLaptopCode} />
+						<h3>Web Projects</h3>
+						<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid.</p>
+					</article>
+					<article>
+						<FontAwesomeIcon icon={faHeart} />
+						<h3>Our history</h3>
+						<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid.</p>
+					</article>
+					<article>
+						<FontAwesomeIcon icon={faCloudArrowDown} />
+						<h3>Development</h3>
+						<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid.</p>
+					</article>
+				</section>
 
-			<section id='lower'>
-				<h2>Meet Our Tim</h2>
-				<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil vel repellat architecto. Provident aut vel nesciunt veritatis in, quis ipsum?</p>
-				<div className='wrap'>
-					{Members.map((member, idx) => {
-						return (
-							<>
-								<article key={idx}>
-									<div className='pic'>
-										<img src={`${process.env.PUBLIC_URL}/img/${member.pic}`} alt={member.name} />
-									</div>
-									<h2>{member.name}</h2>
-									<p>{member.position}</p>
-									<div className='icon'>
-										<FontAwesomeIcon icon={faPhone} />
-										<FontAwesomeIcon icon={faEnvelope} />
-										<FontAwesomeIcon icon={faUser} />
-									</div>
-								</article>
-							</>
-						);
-					})}
-				</div>
-			</section>
+				<section id='lower'>
+					<h2>Meet Our Tim</h2>
+					<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil vel repellat architecto. Provident aut vel nesciunt veritatis in, quis ipsum?</p>
+					<div className='wrap'>
+						{Members.map((member, idx) => {
+							return (
+								<>
+									<article key={idx}>
+										<div className='pic'>
+											<img src={`${process.env.PUBLIC_URL}/img/${member.pic}`} alt={member.name} />
+										</div>
+										<h2>{member.name}</h2>
+										<p>{member.position}</p>
+										<div className='icon'>
+											<FontAwesomeIcon icon={faPhone} />
+											<FontAwesomeIcon icon={faEnvelope} />
+											<FontAwesomeIcon icon={faUser} />
+										</div>
+									</article>
+								</>
+							);
+						})}
+					</div>
+				</section>
+			</div>
 		</Layout>
 	);
 }
